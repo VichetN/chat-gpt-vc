c298d68558c396ac9c729d8fd2be10ce360d8d39
Vichet <vichet.noeng66@gmail.com>
2023-02-11 17:33:32 +0700

    update login button

--- components/Login.jsx
+++ components/Login.jsx
@@ -3,15 +3,17 @@
 import { signIn } from "next-auth/react";
 import Image from "next/image";
 import ChatGPTLogo from "@/assets/images/chatgpt-logo.png";
+import GoogleIcon from "@/assets/images/google-icon.png";
 
 function Login() {
   return (
     <div className="bg-[#11A37F] h-screen flex flex-col items-center justify-center text-center">
       <Image width={300} height={300} src={ChatGPTLogo} alt="logo" />
       <button
-        className="text-white font-bold text-3xl animate-pulse"
+        className="text-black font-bold text-3xl flex justify-center items-center bg-white p-4 gap-3 rounded-lg shadow-lg hover:shadow-none"
         onClick={() => signIn("google")}
       >
+        <Image src={GoogleIcon} width={40} height={40} alt="Google logo" />
         Sign in to use ChatGPT
       </button>
     </div>
